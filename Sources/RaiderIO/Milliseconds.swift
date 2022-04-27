4cdade892918ad8a60d164c004f1732ad42e4ad4
Sören Gade <gade.soeren@gmail.com>
2022-04-27 22:53:25 +0200

    Fix storage of Milliseconds to be of Int type

--- Sources/RaiderIO/Milliseconds.swift
+++ Sources/RaiderIO/Milliseconds.swift
@@ -23,17 +23,19 @@ public struct Milliseconds {
 
 extension Milliseconds: Codable {
 
+    private typealias StorageType = Int64
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
 
-        let value = try container.decode(Int.self)
+        let value = try container.decode(StorageType.self)
         duration = Double(value) / 1000.0
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
 
-        let value = duration * 1000.0
+        let value = StorageType(duration * 1000.0)
         try container.encode(value)
     }
 
